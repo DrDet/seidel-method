cf3f27445ae593d914816078119cb5e1d753403c
denisv <dr.denisv2016@yandex.ru>
2019-05-20 02:06:15 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+# Ax = b
+
+
+def seidel_relaxation_method(A, b, w):
+    rows = len(A)
+    cols = len(A[0])
+
+
+def seidel_method(A, b):
+    return seidel_relaxation_method(A, b, 1)
