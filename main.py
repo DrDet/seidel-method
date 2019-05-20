1dda598ff87f0cefc38d303757f7b89cd81ec555
Golovin Pavel <gopavel0@gmail.com>
2019-05-21 02:38:50 +0300

    implement method without relaxation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import numpy.linalg as linal
+import typing
 
 
 # Ax = b
@@ -7,10 +9,37 @@ def seidel_method(*,
                   A: np.ndarray,
                   b: np.array,
                   w: float = 1,
-                  x0: np.array) -> np.ndarray:
+                  eps: typing.Optional[float] = None,
+                  x0: np.array) -> typing.AsyncGenerator[np.array]:
     """
     Implementation of seidel method.
 
     :return: sequence of approximation.
     """
+    n, m = A.shape
+    assert m == n
+    B = np.zeros(shape=(n, m))
+    c = np.zeros(shape=n)
+    for i in range(0, n):
+        for j in range(0, n):
+            if i != j:
+                B[i][j] = - A[i][j] / A[i][i]
+        c[i] = b[i] / A[i][i]
+
+    if eps is not None:
+        B2 = np.triu(B, 1)
+        eps = (1 - linal.det(B)) / linal.det(B2) * eps
+
+    cur_x = x0
     yield x0
+    while True:
+        next_x = np.zeros(shape=n)
+        for i in range(0, n):
+            for j in range(0, n):
+                if i != j:
+                    next_x[i] += B[i][j] * (next_x[j] if j < i else cur_x[j])
+        yield cur_x
+        if eps is not None:
+            if linal.norm(next_x - cur_x) < eps:
+                break
+        cur_x = next_x
