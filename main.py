0abc70911971927e24177985571d6a0c86759dd3
Golovin Pavel <gopavel0@gmail.com>
2019-05-22 02:47:52 +0300

    add test simple seidel

--- main.py
+++ main.py
@@ -30,8 +30,11 @@ def seidel_method(*,
         c[i] = b[i] / A[i][i]
 
     if eps is not None:
-        B2 = np.triu(B, 1)
-        eps = (1 - linal.norm(B, ord=1)) / linal.norm(B2, ord=1) * eps
+        if linal.norm(B, ord=1) > 1:
+            print('WARNING: ||B|| >= 1 => estimation of eps is invalid')
+        else:
+            B2 = np.triu(B, 1)
+            eps = (1 - linal.norm(B, ord=1)) / linal.norm(B2, ord=1) * eps
 
     cur_x = x0
     yield x0
@@ -51,15 +54,57 @@ def seidel_method(*,
 
 
 if __name__ == '__main__':
-    n = 1
-    A = gen.gen_random_matrix(n)
+    n = 10
+    A0 = gen.gen_diagonally_dominant_matrix(n)
+    A1 = gen.gen_hilbert_matrix(n)
+    A2 = gen.gen_random_matrix(n)
     x0 = np.array(np.random.random(n))
-    b = np.array(np.random.random(n)) * 20
-    print(f'A:\n{A.round(2)},\n'
-          f'b: {b.round(2)},\n'
-          f'x0: {x0.round(2)}')
+    b = np.array(np.random.random(n))
+    print('>>> Initialization')
+    print(f'A0 - diagonally dominant matrix :\n{A0.round(4)},')
+    print(f'A1 - hilbert matrix :\n{A1.round(4)},')
+    print(f'A2 - random matrix :\n{A2.round(4)},')
+    print(f'b:   {b.round(4)},')
+    print(f'x0:  {x0.round(4)}')
+
+    print('\n>>> Test Seidel method on different matrix.')
+    print("\nDiagonally dominant matrix")
+    EPS = 1e-6
+    I = 1000
+    print(f'eps = {EPS}, max_iteration = {I}')
     i = 0
-    for x in islice(seidel_method(A=A, b=b, x0=x0), 10):
+    for x in islice(seidel_method(A=A0, b=b, x0=x0, eps=EPS), I):
         i = i + 1
-        print(f'{i}:{x}')
-    print(f'Ax*: {A.dot(x)}')
+    print('> result')
+    if i >= I:
+        print('Not coverage.')
+    print(f'iteration = {i}')
+    print(f'||A(x*) - b|| = {linal.norm(A0.dot(x) - b)}')
+
+    print("\nHilbert matrix")
+    EPS = 1e-6
+    I = 1000
+    print(f'eps = {EPS}, max_iteration = {I}')
+    i = 0
+    for x in islice(seidel_method(A=A1, b=b, x0=x0, eps=EPS), I):
+        i = i + 1
+    print('> result')
+    if i >= I:
+        print('Not coverage.')
+    print(f'iteration = {i}')
+    print(f'||A(x*) - b|| = {linal.norm(A1.dot(x) - b)}')
+
+    print("\nRandom matrix")
+    EPS = 1e-6
+    I = 100
+    print(f'eps = {EPS}, max_iteration = {I}')
+    i = 0
+    for x in islice(seidel_method(A=A2, b=b, x0=x0, eps=EPS), I):
+        i = i + 1
+    print('> result')
+    if i >= I:
+        print('Not coverage.')
+    print(f'iteration = {i}')
+    print(f'||A(x*) - b|| = {linal.norm(A2.dot(x) - b)}')
+
+
